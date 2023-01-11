8d40a1fa1d0b9421067ca31fe7dcb96ef98978c5
7SilviaT <eagles8485@gmail.com>
2023-01-10 21:42:28 -0500

    fix: message decrypted in shared link

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-const { hash } = window.location;
-
-
 document.querySelector("form").addEventListener("submit", event =>{
   event.preventDefault();
 
   document.querySelector("#message-form").classList.add("hide");
   document.querySelector("#message-link").classList.remove("hide");
 
-  const input = document.querySelector("#message-input");
-  const encryptedMessage = btoa(input.value);
+  const formInput = document.querySelector("#message-form_input");
+  const encryptedMessage = btoa(formInput.value);
 
   const inputLink = document.querySelector("#link-input");
   inputLink.value = `${window.location}#${encryptedMessage}`;
   inputLink.select()
 })
+
+const { hash } = window.location;
+
+const message = atob(hash.replace("#", ""));
+if(message){
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+
+  document.querySelector("h1").innerHTML = message;
+}
